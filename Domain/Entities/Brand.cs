eb2b2d890f7d64d8b9323e385f29768184880404
mustafaguler3 <mustafa.glr@hotmail.com>
2023-08-24 20:12:54 +0300

    project folder created

---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Core.Persistence.Repositories;
+
+namespace Domain.Entities
+{
+	public class Brand : Entity<Guid>
+	{
+		public string Name { get; set; }
+
+		public Brand()
+		{
+
+		}
+
+		public Brand(Guid id,string name)
+		{
+			Id = id;
+			Name = name;
+		}
+	}
+}
+
